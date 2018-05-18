985d88a6e74c53de6015f9126d4427655b947369
Vincent <vincent@expotechinc.com>
2018-05-18 10:52:13 -0700

    Adding getter for isAuthenticatePending

--- src/auth-module/getters.js
+++ src/auth-module/getters.js
@@ -1,5 +1,8 @@
 export default function makeAuthGetters () {
   return {
     // Auth specific "Getters" will go here
+    getAuthenticatePending( state ) {
+      return function () { return state.isAuthenticatePending }
+    }
   }
 }
